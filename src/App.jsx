b8143b23a1959d154a4847eb5af3a24acf6ac947
impronen <impronen@gmail.com>
2023-09-23 18:53:32 +0300

    fix tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <p>DUDE</p>
-    </>
+    <div className="bg-gray-500">
+      <div className="flex flex-row justify-center p-8 hover:py-10">
+        <h1 className="text-3xl font-bold text-regal-blue ">Memory Card</h1>
+      </div>
+    </div>
   );
 }
 
