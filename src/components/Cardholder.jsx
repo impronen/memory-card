b09b91864473cd7a1c67a04a8d80ef6b8d786440
impronen <impronen@gmail.com>
2023-10-01 20:53:57 +0300

    add helper file + notes

--- src/components/Cardholder.jsx
+++ src/components/Cardholder.jsx
@@ -1,9 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function CardHolder() {
-  const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
   const searchTerm = "tundra";
+  const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
   const apiQueryUrl = `https://api.unsplash.com/search/photos?query=${searchTerm}`;
+
+  const [imageStorage, setImageStorage] = useState({
+    imageStorage: [], //TODO: refactor this to be not just for URLs but all image data
+    currentScore: 0,
+    highScore: 0,
+  });
+
   useEffect(() => {
     (async () => {
       try {
@@ -14,16 +21,18 @@ export default function CardHolder() {
           throw new Error(`Request failed, status: ${apiResponse.status}`);
         const imageData = await apiResponse.json();
         console.log(imageData);
+        // helper that chooses 9 random image URL's and populates imageStorage
+        // with objects created from those
       } catch (error) {
         console.error("Something is wrong", error);
       }
     })();
-  });
+  }, [accessKey, apiQueryUrl]);
 
   return (
     <>
       <div>
-        <p>text</p>
+        <p>text new</p>
       </div>
     </>
   );
